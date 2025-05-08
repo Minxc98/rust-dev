973328ae185fb67ba5ef971de914d71be1abfa71
amosmin <amos.min@helium10.com>
2025-05-08 18:22:03 +0800

    更改

--- axum_base/src/controller/user_controller.rs
+++ axum_base/src/controller/user_controller.rs
@@ -11,9 +11,9 @@ pub(crate) async fn update_user(
     State(context): State<AppState>,
     Path(params): Path<i32>,
 ) -> Result<impl IntoResponse, AppError> {
-
+    use crate::model::user::SignInUser;
     let pg_pool = &context.pool;
-    let result = crate::model::user::SignInUser::select_user(pg_pool, params).await?;
+    let result = SignInUser::select_user(pg_pool, params).await?;
     Ok(Json(result))
 }
 
